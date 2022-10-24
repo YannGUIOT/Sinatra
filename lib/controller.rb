40f607bb1bc9ad1051759adf88139a50bc38bd28
YannGUIOT <guiot_yann@hotmail.fr>
2022-10-24 18:17:00 +0200

    sinatra gossip

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,13 +1,45 @@
 require 'gossip'
+require 'comment'
 
 class ApplicationController < Sinatra::Base
   get '/' do
     erb :index, locals: {gossips: Gossip.all}
   end
+
+  get '/gossips/new/' do
+    erb :new_gossip
+  end
+
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"],params["gossip_content"]).save
     redirect '/'
   end
   # les infos des formulaires son mémorisé temporairement dans un hash 'params'
   # le hash se vide à chaque nouvelle requete
+
+
+  # Display Gossip
+  get '/gossips/:id' do
+    erb :show, locals: {id: params[:id].to_i, gossip: Gossip.find((params[:id].to_i)), comments: Comment.select_by_id((params[:id].to_i))}
+  end
+
+  #save new comment
+  post '/gossips/:id' do
+		Comment.new(params[:id], params["comment_author"], params["comment_content"]).save
+    erb :show, locals: {id: params[:id].to_i, gossip: Gossip.all[(params[:id].to_i)], comments: Comment.select_by_id((params[:id].to_i))}
+	end
+
+
+  #permet d'éditer un commentaire
+  get '/gossips/:id/edit/' do
+    erb :edit, locals: {id: params[:id].to_i, gossip: Gossip.all[(params[:id].to_i)]}
+  end
+
+  #sauvegarde et met a jour le fichier csv avec les modifications
+  post '/gossips/:id/edit/' do
+		Gossip.upgrade(params["gossip_author"], params["gossip_content"], (params[:id].to_i))
+		redirect '/'
+  end
+
+
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -22,24 +22,42 @@ class Gossip
     return all_gossips
   end
 
-
-  def self.delete(gossip_to_delete)
-
-    # COPIE les lignes du fichier 'gossip.csv' dans un tableau 'lines'
-    lines = File.readlines('db/gossip.csv')
-    # lines = [ "author,content" , "author, content" , ... ]
-
-    # on supprime du tableau la ligne correspondant à l'index désiré 
-  	lines.delete_at(gossip_to_delete-1)
-    
-    # on supprime toutes les lignes du fichier 'gossip.csv' 
-  	File.open('db/gossip.csv', 'w') {|file| file.truncate(0)}
-    
-    # on réécrit les lignes dans le fichier 'gossip.csv' depuis le tableau 'lines' ne contenant plus l'élément supprimé
-    lines.each do |line|
-      File.open('db/gossip.csv', 'a') { |f| f.write line }
-    end
-
+ #renvoi le gossip correspondant à l'id passé en parametres
+  def self.find(id)
+    return Gossip.all[id]
   end
+
+  def self.upgrade(author, content, id)
+    gossip_array = self.all
+		gossip_array[id.to_i].content = content
+		gossip_array[id.to_i].author = author
+		File.open("./db/gossip.csv", 'w') {|file| file.truncate(0) }
+		gossip_array.each do |gossip|
+			gossip.save
+		end	
+  end
+
+
+
+
+
+  # def self.delete(gossip_to_delete)
+
+  #   # COPIE les lignes du fichier 'gossip.csv' dans un tableau 'lines'
+  #   lines = File.readlines('db/gossip.csv')
+  #   # lines = [ "author,content" , "author, content" , ... ]
+
+  #   # on supprime du tableau la ligne correspondant à l'index désiré 
+  # 	lines.delete_at(gossip_to_delete-1)
+    
+  #   # on supprime toutes les lignes du fichier 'gossip.csv' 
+  # 	File.open('db/gossip.csv', 'w') {|file| file.truncate(0)}
+    
+  #   # on réécrit les lignes dans le fichier 'gossip.csv' depuis le tableau 'lines' ne contenant plus l'élément supprimé
+  #   lines.each do |line|
+  #     File.open('db/gossip.csv', 'a') { |f| f.write line }
+  #   end
+
+  # end
 
 end
